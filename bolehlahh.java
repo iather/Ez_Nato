ae25bf12dd0c91ca87ebd59f281e8e8896169bab
Andi Muh Ridhal A <47064574+iather@users.noreply.github.com>
2020-03-12 22:17:56 +0700

    Delete bolehlahh.java

--- bolehlahh.java
+++
@@ -1,7 +0,0 @@
-public class bolehlahh{
-	public static void main (String[] args){
-		System.out.println("Selamat Belajar");
-		System.out.println("Belajar Bahasa Pemrograman Java");
-		System.out.println("Selesai Belajar Pemrograman Java");
-	}
-}
